48dea622cd7fe2eb740e62b46e7b852d88ae5b5a
musarratChowdhury <muhitlopez143@gmail.com>
2024-06-01 02:37:20 +0600

    expense form added

--- src/app/pages/layout/expense/expense.component.ts
+++ src/app/pages/layout/expense/expense.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {FormBuilder, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-expense',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    ReactiveFormsModule
+  ],
   templateUrl: './expense.component.html',
   styleUrl: './expense.component.css'
 })
 export class ExpenseComponent {
+  constructor(private formBuilder: FormBuilder) {
+  }
 
+  expenseForm = this.formBuilder.group({
+    expenseName: ['', Validators.required],
+    expenseAmount: ['', Validators.min(0)],
+    expenseDate: ['', Validators.required],
+    expenseCategory: ['', Validators.required],
+  });
+
+  onSubmit() {
+    console.log(this.expenseForm.value);
+  }
+
+
+  checkValidity() {
+    return this.expenseForm.status == 'VALID' ? true : false;
+  }
 }
