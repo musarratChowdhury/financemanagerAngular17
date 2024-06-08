ab75662e9259f807429b6665e2230deb30c5bee4
musarratChowdhury <muhitlopez143@gmail.com>
2024-06-08 15:06:08 +0600

    spinner added in login component

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -4,6 +4,8 @@ import { FormsModule } from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
 import { Router } from '@angular/router';
 import { StorageService } from '../../services/storage.service';
+import { createSpinner } from '@syncfusion/ej2-angular-popups';
+import { showSpinner, hideSpinner } from '@syncfusion/ej2-angular-popups';
 
 @Component({
   selector: 'app-login',
@@ -18,6 +20,7 @@ export class LoginComponent implements OnInit {
   isLoginFailed = false;
   errorMessage = '';
   roles: string[] = [];
+  loading: boolean = false;
   constructor(
     private authService: AuthService,
     private storageService: StorageService,
@@ -29,19 +32,29 @@ export class LoginComponent implements OnInit {
     if (this.storageService.isLoggedIn()) {
       this.router.navigate(['/dashboard']);
     }
+    createSpinner({
+      // Specify the target for the spinner to show
+      target: document.getElementById('container') as any,
+    });
   }
 
   onSubmit() {
+    showSpinner((document as any).getElementById('container'));
+    this.loading = true;
     this.authService
       .login(this.loginObj.email, this.loginObj.password)
       .subscribe({
         next: (data) => {
           this.storageService.saveUser(data.token);
           this.router.navigate(['/dashboard']);
+          hideSpinner((document as any).getElementById('container'));
+          this.loading = false;
         },
         error: (err) => {
           this.errorMessage = err.error.message;
           this.isLoginFailed = true;
+          hideSpinner((document as any).getElementById('container'));
+          this.loading = false;
         },
       });
   }
