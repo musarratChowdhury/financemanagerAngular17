be36c2b1d6cd3dfa69302ed41c4e12ed3d403569
musarratChowdhury <muhitlopez143@gmail.com>
2024-06-10 14:10:32 +0600

    ejs license key added

--- src/main.ts
+++ src/main.ts
@@ -4,20 +4,23 @@ import { AppComponent } from './app/app.component';
 import { registerLicense } from '@syncfusion/ej2-base';
 
 registerLicense(
-  "Ngo9BigBOggjHTQxAR8/V1NGaF1cVWhIfEx1RHxQdld5ZFRHallYTnNWUj0eQnxTdEZjUX5ecHNVRWNVVUV1Xw=="
+  'ORg4AjUWIQA/Gnt2UFhhQlJBfV5AQmBIYVp/TGpJfl96cVxMZVVBJAtUQF1hTX5Vd0VjWnxXcn1UQmNd'
 );
 
 // Wait until the DOM is fully loaded
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener('DOMContentLoaded', function () {
   // Get the element by its style attribute
-  var element:HTMLElement|null = document.querySelector('div[style*="position: fixed"]');
+  var element: HTMLElement | null = document.querySelector(
+    'div[style*="position: fixed"]'
+  );
 
   // Check if the element exists
   if (element) {
     // Hide the element by setting its display to none
-    element.style.display = "none";
+    element.style.display = 'none';
   }
 });
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, appConfig).catch((err) =>
+  console.error(err)
+);
