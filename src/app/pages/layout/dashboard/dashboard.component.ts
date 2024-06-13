7b1430c00eace97849fc3721a0bf370e99485018
musarratChowdhury <muhitlopez143@gmail.com>
2024-06-13 13:52:53 +0600

    update on dashboard

--- src/app/pages/layout/dashboard/dashboard.component.ts
+++ src/app/pages/layout/dashboard/dashboard.component.ts
@@ -27,7 +27,12 @@ import {
   AccumulationAnnotationService,
   AccumulationDataLabelService,
 } from '@syncfusion/ej2-angular-charts';
-
+import { DashboardService } from '../../../services/dashboard.service';
+interface PieData {
+  x: string;
+  y: number;
+  text: string;
+}
 @Component({
   selector: 'app-dashboard',
   standalone: true,
@@ -61,7 +66,7 @@ import {
   ],
 })
 export class DashboardComponent implements OnInit {
-  public piedata?: Object[];
+  public piedata?: PieData[];
   public legendSettings?: Object;
   public tooltip?: Object;
   public title?: string;
@@ -70,26 +75,47 @@ export class DashboardComponent implements OnInit {
   public barChartTitle?: string;
   primaryXAxis: any;
   primaryYAxis: any;
+  TotalCostOccuredThisMonth: number = 0;
+
+  constructor(private dashboardService: DashboardService) {}
 
   public tooltipRender(args: IAccTooltipRenderEventArgs): void {
     let value = (args.point.y / args.series.sumOfPoints) * 100;
     args['text'] = args.point.x + ' : ' + Math.ceil(value) + '' + '%';
   }
   ngOnInit(): void {
-    this.piedata = [
-      { x: 'Jan', y: 3, text: 'Jan: 3' },
-      { x: 'Feb', y: 3.5, text: 'Feb: 3.5' },
-      { x: 'Mar', y: 7, text: 'Mar: 7' },
-      { x: 'Apr', y: 13.5, text: 'Apr: 13.5' },
-      { x: 'May', y: 19, text: 'May: 19' },
-      { x: 'Jun', y: 23.5, text: 'Jun: 23.5' },
-      { x: 'Jul', y: 26, text: 'Jul: 26' },
-      { x: 'Aug', y: 25, text: 'Aug: 25' },
-      { x: 'Sep', y: 21, text: 'Sep: 21' },
-      { x: 'Oct', y: 15, text: 'Oct: 15' },
-      { x: 'Nov', y: 9, text: 'Nov: 9' },
-      { x: 'Dec', y: 3.5, text: 'Dec: 3.5' },
-    ];
+    var x = this.dashboardService.get().subscribe({
+      next: (data: any[]) => {
+        console.log(data);
+        this.piedata = data.map((category) => ({
+          x: category.categoryName,
+          y: category.totalAmount,
+          text: `${category.categoryName}: ${category.totalAmount}`,
+        }));
+        this.TotalCostOccuredThisMonth = this.piedata.reduce(
+          (sum, item: any) => sum + item.y,
+          0
+        );
+        this.title = `Total Cost Occurred This Month: ${this.TotalCostOccuredThisMonth}`;
+        console.log(this.TotalCostOccuredThisMonth);
+      },
+      error: (err) => console.log(err),
+    });
+
+    // this.piedata = [
+    //   { x: 'Jan', y: 3, text: 'Jan: 3' },
+    //   { x: 'Feb', y: 3.5, text: 'Feb: 3.5' },
+    //   { x: 'Mar', y: 7, text: 'Mar: 7' },
+    //   { x: 'Apr', y: 13.5, text: 'Apr: 13.5' },
+    //   { x: 'May', y: 19, text: 'May: 19' },
+    //   { x: 'Jun', y: 23.5, text: 'Jun: 23.5' },
+    //   { x: 'Jul', y: 26, text: 'Jul: 26' },
+    //   { x: 'Aug', y: 25, text: 'Aug: 25' },
+    //   { x: 'Sep', y: 21, text: 'Sep: 21' },
+    //   { x: 'Oct', y: 15, text: 'Oct: 15' },
+    //   { x: 'Nov', y: 9, text: 'Nov: 9' },
+    //   { x: 'Dec', y: 3.5, text: 'Dec: 3.5' },
+    // ];
     this.primaryXAxis = {
       valueType: 'Category',
       title: 'Months',
@@ -109,6 +135,6 @@ export class DashboardComponent implements OnInit {
     this.tooltip = {
       enable: true,
     };
-    this.title = 'Category Based Costs';
+    this.title = `Total Cost Occured This month : ${this.TotalCostOccuredThisMonth}`;
   }
 }
---
+++ src/app/services/dashboard.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { environment } from '../../environments/environment';
+
+const API_URL = `${environment.apiUrl}/api/Expense/`;
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json',
+  }),
+};
+@Injectable({
+  providedIn: 'root',
+})
+export class DashboardService {
+  constructor(private http: HttpClient) {}
+
+  // Read an item by ID
+  get(): Observable<any> {
+    return this.http
+      .get(`${API_URL}GetExpensesOfThisMonth`, httpOptions)
+      .pipe(catchError(this.handleError));
+  }
+
+  private handleError(error: any): Observable<never> {
+    console.error('An error occurred:', error); // for demo purposes only
+    return error('Something bad happened; please try again later.');
+  }
+}
